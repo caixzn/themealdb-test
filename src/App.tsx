f4bf1f13508e8f22645f71b1e65f0a557017da68
caio <hi@caio.sh>
2023-10-07 04:12:29 -0300

    added data types

--- src/App.tsx
+++ src/App.tsx
@@ -222,3 +222,27 @@ export function RecipeCard({ recipe }: { recipe: Recipe }) {
     </div>
   )
 }
+
+type Recipe = {
+  idMeal: string;
+  strMeal: string;
+  strDrinkAlternate: string | null;
+  strCategory: string | null;
+  strArea: string | null;
+  strInstructions: string | null;
+  strMealThumb: string | null;
+  strTags: string | null;
+  strYoutube: string | null;
+  strSource: string | null;
+  strImageSource: string | null;
+  strCreativeCommonsConfirmed: string | null;
+  dateModified: string | null;
+} & Record<`${'strMeasure' | 'strIngredient'}${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20}`, string | null>;
+
+type Ingredient = {
+  idIngredient: string;
+  strIngredient: string;
+  strDescription: string | null;
+  strType: string | null;
+}
+
